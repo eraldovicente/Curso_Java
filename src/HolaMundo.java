70b506e8d506f5e0342e0b4743b5d25640d3fa4e
Eraldo Vicente <eraldovicente2@gmail.com>
2021-03-18 00:25:19 -0300

    Variables en Java

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,6 +1,7 @@
 // Mi clase en Java
 public class HolaMundo {
     public static void main(String argrs[]) {
-        System.out.println("Hola mundo desde Java");
-    }
+        int miVariableEntera = 10;
+        System.out.println(miVariableEntera);
+    } 
 }

841e93e754b6c70636091f422a5d26fc9a348814
Eraldo Vicente <eraldovicente2@gmail.com>
2021-03-18 01:00:39 -0300

    Tipos enteros y tipo string en Java

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,7 +1,19 @@
 // Mi clase en Java
 public class HolaMundo {
     public static void main(String argrs[]) {
+        
+        //Definimos la variable
         int miVariableEntera = 10;
         System.out.println(miVariableEntera);
+        
+        //Modificamos el valor de la variable
+        miVariableEntera = 5;
+        System.out.println(miVariableEntera);
+        
+        String miVariableCadena = "Saludos";
+        System.out.println(miVariableCadena);
+        
+        miVariableCadena = "Hasta la vista, Baby!";
+        System.out.println(miVariableCadena);
     } 
 }

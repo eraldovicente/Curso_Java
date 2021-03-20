e3056786254b6d32e8cea84f5b045224ab24a9d1
Eraldo Vicente <eraldovicente2@gmail.com>
2021-03-20 19:27:46 -0300

    Tipos primitivos flotantes

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -8,27 +8,18 @@ public class HolaMundo {
     public static void main(String argrs[]) { 
 
         /*
-            Tipos primitivos enteros: byte, short, int, long
+            Tipos primitivos flotante: float y double
         */
         
-        byte numeroByte = (byte)129;
-        System.out.println("Valor byte:" + numeroByte);
-        System.out.println("Valor minimo byte:" + Byte.MIN_VALUE);
-        System.out.println("Valor maximo byte:" + Byte.MAX_VALUE);
+        float numeroFloat = (float)3.4028235E38F;
+        System.out.println("numeroFloat = " + numeroFloat);
+        System.out.println("Valor minimo tipo float: " + Float.MIN_VALUE);        
+        System.out.println("Valor maximo tipo float: " + Float.MAX_VALUE);
         
-        short numeroShort = (short)32768;
-        System.out.println("numeroShort = " + numeroShort);
-        System.out.println("Valor minimo short:" + Short.MIN_VALUE);
-        System.out.println("Valor maximo short:" + Short.MAX_VALUE);
-        
-        int numeroInt = (int)2147483648L;
-        System.out.println("numeroInt = " + numeroInt);
-        System.out.println("Valor minimo int:" + Integer.MIN_VALUE);
-        System.out.println("Valor maximo int:" + Integer.MAX_VALUE);
-        
-        long numeroLong = 9223372036854775807L;
-        System.out.println("numeroLong = " + numeroLong);
-        System.out.println("Valor minimo long:" + Long.MIN_VALUE);
-        System.out.println("Valor maximo long:" + Long.MAX_VALUE);
+        double numeroDouble = 1.7976931348623157E308;
+        System.out.println("numeroDouble = " + numeroDouble);
+        System.out.println("Valor minimo tipo double: " + Double.MIN_VALUE);        
+        System.out.println("Valor maximo tipo double: " + Double.MAX_VALUE);
+
     }
 }

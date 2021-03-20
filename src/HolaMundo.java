7dfae0c466bd77fa36e54e4236366957382977b3
Eraldo Vicente <eraldovicente2@gmail.com>
2021-03-19 23:32:58 -0300

    Tipos primitivos en Java

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -5,14 +5,15 @@ import java.util.Scanner;
 
 public class HolaMundo {
 
-    public static void main(String argrs[]) {
+    public static void main(String argrs[]) { 
 
-        System.out.println("Escribe tu nombre: ");
-        Scanner consola = new Scanner(System.in);
-        var usuario = consola.nextLine();
-        System.out.println("usuario = " + usuario);
-        System.out.println("Escribe el titulo: ");
-        var titulo = consola.nextLine();
-        System.out.println("Resultado: " + titulo + " " + usuario);
+        /*
+            Tipos primitivos enteros: byte, short, int, long
+        */
+        
+        byte numeroByte = (byte)129;
+        System.out.println("Valor byte:" + numeroByte);
+        System.out.println("Valor minimo byte:" + Byte.MIN_VALUE);
+        System.out.println("Valor maximo byte:" + Byte.MAX_VALUE);
     }
 }
